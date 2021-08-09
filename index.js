bbc68d30fc3306c6b76a554b02175e674159097c
Raghav Maheshwari <68119293+rsmaheshwari@users.noreply.github.com>
2021-08-09 08:55:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+function showMenu() {
+	if(document.getElementById('mobile_menu').style.display == 'none')
+		document.getElementById('mobile_menu').style.display = 'block';
+	else
+		document.getElementById('mobile_menu').style.display = 'none';
+}
+
